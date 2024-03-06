217ca02a70144f990746fbd1295fe1567a1a548d
LavrovNick <lavrovmykola@gmail.com>
2024-03-06 09:36:05 +0200

    Initial Commit

---
+++ OpenAIUser/OpenAIUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAIUserApp.swift
+//  OpenAIUser
+//
+//  Created by Nick Lavrov on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenAIUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
